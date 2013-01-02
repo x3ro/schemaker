936581ec48cc8c1c9dd269621bd9eb2050610958
Claus Due <claus@wildside.dk>
2013-01-02 17:55:38 +0100

    [TASK] Allow caching of FE plugin

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,5 +7,5 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['schemaker']['setup'] = unserialize($_EXT
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_Schemaker_Command_SchemaCommandController';
 
 if (isset($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['schemaker']['setup']['frontend']) && $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['schemaker']['setup']['frontend'] > 0) {
-	Tx_Extbase_Utility_Extension::configurePlugin($_EXTKEY, 'Schema', array('Schema' => 'schema'), array('Schema' => 'schema'));
+	Tx_Extbase_Utility_Extension::configurePlugin($_EXTKEY, 'Schema', array('Schema' => 'schema'), array());
 }
