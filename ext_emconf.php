9f3942397daaf242cec43956d5a95d2217d5d318
Claus Due <claus@wildside.dk>
2013-01-09 01:12:06 +0100

    [TER] 2.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "schemaker".
  *
- * Auto generated 18-11-2012 20:36
+ * Auto generated 09-01-2013 01:11
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '2.0.0',
+	'version' => '2.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5-0.0.0',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:16:"ext_autoload.php";s:4:"f55e";s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"a63e";s:9:"README.md";s:4:"07b9";s:43:"Classes/Command/SchemaCommandController.php";s:4:"7556";s:33:"Classes/Service/SchemaService.php";s:4:"2c55";}',
+	'_md5_values_when_last_written' => 'a:10:{s:16:"ext_autoload.php";s:4:"f55e";s:21:"ext_conf_template.txt";s:4:"456a";s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"57e0";s:14:"ext_tables.php";s:4:"809f";s:9:"README.md";s:4:"07b9";s:43:"Classes/Command/SchemaCommandController.php";s:4:"7556";s:39:"Classes/Controller/SchemaController.php";s:4:"2e41";s:33:"Classes/Service/SchemaService.php";s:4:"2c55";s:46:"Resources/Private/Templates/Schema/Schema.html";s:4:"dfe1";}',
 );
 
 ?>
