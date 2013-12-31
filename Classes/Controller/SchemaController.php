228a6825f92e1fc2546cb08b6ff9b0521e39ee5e
Claus Due <claus@namelesscoder.net>
2013-12-31 21:46:49 +0100

    [TASK] Happy new year!

--- Classes/Controller/SchemaController.php
+++ Classes/Controller/SchemaController.php
@@ -3,7 +3,7 @@ namespace FluidTYPO3\Schemaker\Controller;
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2012 Claus Due <claus@wildside.dk>, Wildside A/S
+ *  (c) 2014 Claus Due <claus@namelesscoder.net>
  *
  *  All rights reserved
  *
