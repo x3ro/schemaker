59ba0c7d1d9d9ad38bac7fa0ee9479b9e057fb9f
Claus Due <claus@namelesscoder.net>
2014-04-26 04:58:56 +0200

    [TER] 3.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "schemaker".
  *
- * Auto generated 09-01-2013 01:11
+ * Auto generated 26-04-2014 04:58
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '2.1.0',
+	'version' => '3.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.1.0-6.2.99',
@@ -40,12 +40,12 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'vhs' => ''
+			'vhs' => '',
 		),
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:16:"ext_autoload.php";s:4:"f55e";s:21:"ext_conf_template.txt";s:4:"456a";s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"57e0";s:14:"ext_tables.php";s:4:"809f";s:9:"README.md";s:4:"07b9";s:43:"Classes/Command/SchemaCommandController.php";s:4:"7556";s:39:"Classes/Controller/SchemaController.php";s:4:"2e41";s:33:"Classes/Service/SchemaService.php";s:4:"2c55";s:46:"Resources/Private/Templates/Schema/Schema.html";s:4:"dfe1";}',
+	'_md5_values_when_last_written' => 'a:26:{s:13:"composer.json";s:4:"d601";s:16:"ext_autoload.php";s:4:"f55e";s:21:"ext_conf_template.txt";s:4:"cef1";s:12:"ext_icon.gif";s:4:"68b4";s:17:"ext_localconf.php";s:4:"949d";s:14:"ext_tables.php";s:4:"07a3";s:10:"LICENSE.md";s:4:"c813";s:9:"README.md";s:4:"2b01";s:22:"Build/ImportSchema.sql";s:4:"c524";s:28:"Build/LocalConfiguration.php";s:4:"0afc";s:23:"Build/PackageStates.php";s:4:"7921";s:43:"Classes/Command/SchemaCommandController.php";s:4:"427e";s:39:"Classes/Controller/SchemaController.php";s:4:"305d";s:33:"Classes/Service/SchemaService.php";s:4:"4d22";s:34:"Configuration/TypoScript/setup.txt";s:4:"1a18";s:33:"Documentation/ComplexityChart.png";s:4:"9a0f";s:30:"Documentation/PyramidChart.png";s:4:"b3db";s:42:"Resources/Private/Partials/BreadCrumb.html";s:4:"7c6c";s:37:"Resources/Private/Partials/Class.html";s:4:"a110";s:37:"Resources/Private/Partials/Index.html";s:4:"8ccf";s:36:"Resources/Private/Partials/Link.html";s:4:"8ebd";s:40:"Resources/Private/Partials/Overview.html";s:4:"e5f4";s:40:"Resources/Private/Scripts/GithubHook.php";s:4:"76be";s:46:"Resources/Private/Templates/Schema/Schema.html";s:4:"0d94";s:42:"Resources/Public/Javascript/Application.js";s:4:"0253";s:43:"Resources/Public/Stylesheet/Application.css";s:4:"b04d";}',
 );
 
 ?>
