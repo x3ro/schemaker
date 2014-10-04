e6e2ea8a5dc4f48b3a72375fde08f2910861094e
Claus Due <claus@namelesscoder.net>
2014-10-04 13:28:36 +0200

    [TER] 3.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '3.0.1',
+	'version' => '3.0.2',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.1.0-6.2.99',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'conflicts' => array(
 		),
 		'suggests' => array(
-			'vhs' => '',
+			'vhs' => '1.9.0-2.1.99',
 		),
 	),
 	'suggests' => array(
